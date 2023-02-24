810ee0758e2f60160f07ad9f25869f86fb84dec1
Rahmad <rahmatjohan37@gmail.com>
2023-02-24 23:00:07 +0800

    add the key props

--- components/navbar.js
+++ components/navbar.js
@@ -16,9 +16,9 @@ const  navs = [
         <Link href="/" className="Logo">Rahmad.</Link>
 
         <ul className="nav-links">
-          {navs.map(nav => ( 
-          <li><Link href={nav.href} className={`nav-item ${ router.pathname == nav.href ?  'active' : ''}`}>{nav.text}</Link></li> 
-          ))}
+  {navs.map((nav, index) => ( 
+    <li key={index}><Link href={nav.href} className={`nav-item ${ router.pathname == nav.href ?  'active' : ''}`}>{nav.text}</Link></li> 
+))}
         </ul>
       </div>
      </nav>

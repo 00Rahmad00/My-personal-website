8328c507b02acff998ccefaa8d5943197c4d6d3a
Rahmad <rahmatjohan37@gmail.com>
2023-02-26 12:42:34 +0800

    change a lot

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -4,44 +4,65 @@ import Image from "next/image";
 import Project1 from "../public/1.jpg";
 import Project2 from "../public/2.jpg";
 import Footer from "../components/Footer";
+import { FaGithub, FaLink } from "react-icons/fa";
 
 function Portfolio(){
     return(
         <>
-
         <Head>
           <title>Portfolio</title>
         </Head>
-
             <Navbar />
-        <section className="hero">
+        <section className="hero portfolio">
          <div className="container">
           <div className="text-wrapper w-full">
             <h1 className="title">Portfolio</h1>
-            <p className="description">Incididunt in cillum magna occaecat nisi qui in exercitation labore anim adipisicing amet irure tempor id pariatur nostrud labore nulla veniam.</p>
+            <p className="description">Dibawah ini adalah beberapa portfolio yang telah saya buat :</p>
 
             <div className="portfolio-wrapper">
                 <div className="portfolio-item">
                     <Image src={Project1} className="portfolio-image" alt="" />
-
                         <h4 className="portfolio-name">E-commerce Website</h4>
                             <div className="portfolio-category">Web Dev</div>
+                            <div className="portfolio-icons">
+                                <a href="https://github.com" target="_blank" rel="noopener noreferrer"><FaGithub className="icon github-icon" /></a>
+                                <a href="https://www.example.com" target="_blank" rel="noopener noreferrer"><FaLink className="icon link-icon" /></a>
+                            </div>
                 </div>
                  <div className="portfolio-item">
                    <Image src={Project2} className="portfolio-image"  alt=""/>
-
                         <h4 className="portfolio-name">Chat Website</h4>
                             <div className="portfolio-category">Web Dev</div>
+                            <div className="portfolio-icons">
+                                <a href="https://github.com" target="_blank" rel="noopener noreferrer"><FaGithub className="icon github-icon" /></a>
+                                <a href="https://www.example.com" target="_blank" rel="noopener noreferrer"><FaLink className="icon link-icon" /></a>
+                            </div>
+                </div>
+                 <div className="portfolio-item">
+                   <Image src={Project2} className="portfolio-image"  alt=""/>
+                        <h4 className="portfolio-name">Chat Website</h4>
+                            <div className="portfolio-category">Web Dev</div>
+                            <div className="portfolio-icons">
+                                <a href="https://github.com" target="_blank" rel="noopener noreferrer"><FaGithub className="icon github-icon" /></a>
+                                <a href="https://www.example.com" target="_blank" rel="noopener noreferrer"><FaLink className="icon link-icon" /></a>
+                            </div>
+                </div>
+                 <div className="portfolio-item">
+                   <Image src={Project2} className="portfolio-image"  alt=""/>
+                        <h4 className="portfolio-name">Chat Website</h4>
+                            <div className="portfolio-category">Web Dev</div>
+                            <div className="portfolio-icons">
+                                <a href="https://github.com" target="_blank" rel="noopener noreferrer"><FaGithub className="icon github-icon" /></a>
+                                <a href="https://www.example.com" target="_blank" rel="noopener noreferrer"><FaLink className="icon link-icon" /></a>
+                            </div>
                 </div>
             </div>
-		</div>
-	        </div>
-			</section>
-
+        </div>
+            </div>
+            </section>
                <Footer />
-		</>
-	);
+        </>
+    );
 }
 
 export default Portfolio;
-

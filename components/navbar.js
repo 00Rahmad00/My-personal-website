2e4b1cf35cf3093fa3bf9c3423c61e4c32f0e704
Rahmad <rahmatjohan37@gmail.com>
2023-02-28 02:21:24 +0800

    cahnge a lot

--- components/navbar.js
+++ components/navbar.js
@@ -1,5 +1,10 @@
 import Link from "next/link";
 import { withRouter } from "next/router";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import { useState } from "react"; // tambahkan import useState dari react
 
 function Navbar({ router }) {
   const navs = [
@@ -9,29 +14,44 @@ function Navbar({ router }) {
     { text: "Contact", href: "/contact" },
   ];
 
+  // tambahkan useState untuk membuat state
+  const [clicked, setClicked] = useState(false);
+
+  // ubah handleClicked ke arrow function
+  const handleClicked = () => {
+    setClicked(!clicked);
+  };
+
   return (
-    <nav className="navbar">
-      <div className="container">
+    <>
+      <nav className="navbar">
         <Link href="/" className="Logo">
           Rahmad.
         </Link>
-
-        <ul className="nav-links">
-          {navs.map((nav, index) => (
-            <li key={index}>
-              <Link
-                href={nav.href}
-                className={`nav-item ${
-                  router.pathname === nav.href ? "active" : ""
-                }`}
-              >
-                {nav.text}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </nav>
+        <div>
+          <ul className="navbar">
+            {navs.map((nav, index) => (
+              <li key={index}>
+                <Link
+                  href={nav.href}
+                  className={`nav-item ${
+                    router.pathname === nav.href ? "active" : ""
+                  }`}
+                >
+                  {nav.text}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="mobile" onClick={handleClicked}>
+          <FontAwesomeIcon
+            icon={clicked ? faTimes : faBars}
+            className="fa-icon"
+          />
+        </div>
+      </nav>
+    </>
   );
 }
 
